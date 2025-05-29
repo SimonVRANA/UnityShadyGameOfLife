18ecafbeba94d04b14899ff4a224168c0c0f8a7b
Simon Vrana <simon.vrana.pro@gmail.com>
2025-05-29 08:59:40 +0200

    [Fct] Added a bunch of tests for IImageService and added some safety to all ImageService methods.

--- Assets/Scripts/Services/ImageService/ImageService.cs
+++ Assets/Scripts/Services/ImageService/ImageService.cs
@@ -19,7 +19,13 @@ namespace SGOL.Services.Image
 			get => numberOfColumns;
 			set
 			{
-				numberOfColumns = AdjustColumnsNumber(value);
+				int adjustedValue = AdjustColumnsNumber(value);
+				if (adjustedValue == numberOfColumns)
+				{
+					return;
+				}
+
+				numberOfColumns = adjustedValue;
 
 				Texture2D newTexture = new(numberOfColumns, (int)Math.Floor(numberOfColumns * IImageService.columnsToRowsRatio))
 				{
@@ -40,6 +46,11 @@ namespace SGOL.Services.Image
 
 		public ImageService(UnityEngine.UI.Image image)
 		{
+			if (image == null)
+			{
+				throw new ArgumentNullException(nameof(image), "Image cannot be null.");
+			}
+
 			gameImage = image;
 
 			gameImage.color = Color.white;
@@ -47,6 +58,11 @@ namespace SGOL.Services.Image
 
 		public void ApplyShader(Material material)
 		{
+			if (material == null)
+			{
+				throw new ArgumentNullException(nameof(material), "Material cannot be null.");
+			}
+
 			RenderTexture tmpTexture = RenderTexture.GetTemporary(gameImage.sprite.texture.width, gameImage.sprite.texture.height);
 			Graphics.Blit(gameImage.sprite.texture, tmpTexture, material);
 			RenderTexture.active = tmpTexture;
@@ -57,6 +73,11 @@ namespace SGOL.Services.Image
 
 		public void RandomizePixels(Color deadColor, Color aliveColor, float aliveRatio)
 		{
+			if (aliveRatio < 0 || aliveRatio > 1)
+			{
+				throw new ArgumentOutOfRangeException(nameof(aliveRatio), "Alive ratio must be between 0 and 1.");
+			}
+
 			System.Random random = new();
 
 			for (int widthIndex = 0; widthIndex < gameImage.sprite.texture.width; widthIndex++)
@@ -71,19 +92,37 @@ namespace SGOL.Services.Image
 
 		public Vector2 ImagePositionToPixelPosition(Vector2 imagePosition)
 		{
-			int x = (int)Math.Floor(imagePosition.x * NumberOfColumns);
+			if (imagePosition.x < 0 || imagePosition.x > 1
+			   || imagePosition.y < 0 || imagePosition.y > 1)
+			{
+				throw new ArgumentOutOfRangeException(nameof(imagePosition), "Image position must be between 0 and 1 for both x and y.");
+			}
+
+			int x = (int)Math.Floor(imagePosition.x * (NumberOfColumns - 1));
 			int numberOfRows = (int)Math.Floor(numberOfColumns * IImageService.columnsToRowsRatio);
-			float y = (int)Math.Floor(imagePosition.y * numberOfRows);
+			float y = (int)Math.Floor(imagePosition.y * (numberOfRows - 1));
 			return new Vector2(x, y);
 		}
 
 		public Color GetPixelColor(Vector2 pixelPosition)
 		{
+			if (pixelPosition.x < 0 || pixelPosition.x >= gameImage.sprite.texture.width
+			   || pixelPosition.y < 0 || pixelPosition.y >= gameImage.sprite.texture.height)
+			{
+				throw new ArgumentOutOfRangeException(nameof(pixelPosition), "Pixel position must be within the texture bounds.");
+			}
+
 			return gameImage.sprite.texture.GetPixel((int)Math.Floor(pixelPosition.x), (int)Math.Floor(pixelPosition.y));
 		}
 
 		public void SetPixelColor(Vector2 pixelPosition, Color color)
 		{
+			if (pixelPosition.x < 0 || pixelPosition.x >= gameImage.sprite.texture.width
+			   || pixelPosition.y < 0 || pixelPosition.y >= gameImage.sprite.texture.height)
+			{
+				throw new ArgumentOutOfRangeException(nameof(pixelPosition), "Pixel position must be within the texture bounds.");
+			}
+
 			gameImage.sprite.texture.SetPixel((int)Math.Floor(pixelPosition.x), (int)Math.Floor(pixelPosition.y), color);
 			gameImage.sprite.texture.Apply();
 		}
